d07bc390cf83ded33d7e21094e943d05529c0bf2
Garrett Koller <gkoller@catalist.us>
2021-04-11 10:06:56 -0400

    Major: Reworked code from magtag_mqtt_remote to instead make the MagTag display the time elapsed since a timestamp from an MQTT topic

--- Makefile
+++ Makefile
@@ -5,25 +5,25 @@ PROD_LIB = adafruit-circuitpython-bundle-6.x-mpy-20210314/lib
 DEV_LIB = adafruit-circuitpython-bundle-py-20210321/lib
 
 MODULE_DIRS = adafruit_bitmap_font adafruit_display_text adafruit_io adafruit_magtag adafruit_minimqtt adafruit_portalbase
-MODULE_FILES = adafruit_fakerequests adafruit_requests adafruit_logging neopixel simpleio
+MODULE_FILES = adafruit_datetime adafruit_fakerequests adafruit_logging adafruit_requests neopixel simpleio
 
 code.py: FORCE
-	cp code.py "$(TARGET)/code.py"
+	rsync -avh code.py "$(TARGET)/code.py"
 
 prod: code.py
 	for dir in $(MODULE_DIRS); do \
-		cp -rfv "$(PROD_LIB)/$$dir" "$(TARGET)/lib/" ; \
+		rsync -avh "$(PROD_LIB)/$$dir" "$(TARGET)/lib/" ; \
 	done
 	for file in $(MODULE_FILES); do \
-		cp -fv "$(PROD_LIB)/$$file.mpy" "$(TARGET)/lib/" ; \
+		rsync -avh "$(PROD_LIB)/$$file.mpy" "$(TARGET)/lib/" ; \
 	done
 
 dev: code.py
 	for dir in $(MODULE_DIRS); do \
-		cp -rfv "$(DEV_LIB)/$$dir" "$(TARGET)/lib/" ; \
+		rsync -avh "$(DEV_LIB)/$$dir" "$(TARGET)/lib/" ; \
 	done
 	for file in $(MODULE_FILES); do \
-		cp -fv "$(DEV_LIB)/$$file.py" "$(TARGET)/lib/" ; \
+		rsync -avh "$(DEV_LIB)/$$file.py" "$(TARGET)/lib/" ; \
 	done
 
 all: prod
--- code.py
+++ code.py
@@ -3,8 +3,8 @@
 # SPDX-License-Identifier: Unlicense
 
 
-import sys
 import time
+import adafruit_datetime as datetime
 
 # noinspection PyUnresolvedReferences
 import socketpool
@@ -26,7 +26,8 @@ except ImportError:
     raise
 
 
-MQTT_TOPIC = "perupino/garrett/temperatureF"
+MQTT_TOPIC_OUT = "dogs/last_time_out"
+MQTT_TOPIC_NOW = "time/now"
 SSID = secrets["ssid"]
 HOSTNAME = secrets["broker"]
 PORT = secrets["port"]
@@ -41,23 +42,14 @@ def main():
     )
 
     try:
-        magtag.peripherals.neopixel_disable = False
-        magtag.peripherals.neopixels.brightness = 0.01
-        magtag.peripherals.neopixels.fill((0, 0, 0))
-        magtag.peripherals.neopixels[3] = (0xFF, 0xFF, 0xFF)
+        magtag.peripherals.neopixel_disable = True
     except Exception as neopixel_ex:
-        logger.error("Failed to init NeoPixels and set #1 to white; skipping")
+        logger.error("Failed to disable NeoPixels during init; skipping")
         logger.error("%s: %s", type(neopixel_ex).__name__, neopixel_ex.args)
 
     logger.info("WiFi connecting to %s", SSID)
     magtag.network.connect()
     logger.info("WiFi connected to %s", SSID)
-
-    try:
-        magtag.peripherals.neopixels[2] = (0xFF, 0xFF, 0xFF)
-    except Exception as neopixel_ex:
-        logger.error("Failed to set NeoPixel #2 to white after connecting to WiFi; skipping")
-        logger.error("%s: %s", type(neopixel_ex).__name__, neopixel_ex.args)
 
     magtag.add_text(
         text_position=(
@@ -69,32 +61,51 @@ def main():
         is_data=False,
     )
 
-    previous_temp = None
+    time_out = None
+    time_now = None
+    screen_light_on = False
 
     # Define callback methods which are called when events occur
     # pylint: disable=unused-argument
     def connected(client, userdata, flags, rc):
         # This function will be called when the client is connected successfully to the broker.
         logger.debug("Connected to MQTT broker at %s:%d", HOSTNAME, PORT)
-        logger.debug("Listening for topic changes on %s", MQTT_TOPIC)
+        logger.debug("Listening for topic changes on %s", MQTT_TOPIC_OUT)
         # Subscribe to all changes on the desired topic.
-        client.subscribe(MQTT_TOPIC)
+        client.subscribe(MQTT_TOPIC_OUT)
+        logger.debug("Listening for topic changes on %s", MQTT_TOPIC_NOW)
+        client.subscribe(MQTT_TOPIC_NOW)
 
     def disconnected(client, userdata, rc):
         # This method is called when the client is disconnected
         logger.warning("Disconnected from MQTT broker %s:%d", HOSTNAME, PORT)
-        magtag.peripherals.neopixels[0] = (0xFF, 0, 0)
 
     def message(client, topic, msg_text):
         # This method is called when a topic the client is subscribed to has a new message.
         logger.debug("New message on topic %s: %s", topic, msg_text)
-        nonlocal previous_temp
-        if msg_text != previous_temp:
-            previous_temp = msg_text
-            magtag.set_text(msg_text)
-        else:
-            logger.debug("Skipping screen redraw since temperature is the same")
-        magtag.peripherals.neopixels[0] = (0xFF, 0xFF, 0xFF)
+        nonlocal time_out
+        nonlocal time_now
+        nonlocal screen_light_on
+        if topic == MQTT_TOPIC_OUT:
+            time_out = datetime.datetime.fromisoformat(msg_text)
+            logger.debug("Received out string: %s", time_out)
+        elif topic == MQTT_TOPIC_NOW:
+            time_now = datetime.datetime.fromisoformat(msg_text)
+            logger.debug("Received now string: %s", time_now)
+        if time_out and time_now:
+            delta_time = time_now - time_out
+            logger.debug("Delta: %s", delta_time)
+            total_seconds = delta_time.seconds
+            hours = total_seconds // (60 * 60)
+            total_seconds = total_seconds - (hours * 60 * 60)
+            minutes = total_seconds // 60
+            delta_str = f"{hours} hr {minutes} min"
+            magtag.set_text(delta_str)
+            if not screen_light_on and hours > 2:
+                screen_light_on = True
+                magtag.peripherals.neopixel_disable = False
+                magtag.peripherals.neopixels.brightness = 0.01
+                magtag.peripherals.neopixels.fill((0xFF, 0xFF, 0xFF))
 
     # Create a socket pool
     pool = socketpool.SocketPool(wifi.radio)
@@ -124,40 +135,28 @@ def main():
     # NOTE: NO code below this loop will execute
     # NOTE: Network reconnection is handled within this loop
     while True:
-        magtag.peripherals.neopixels[1] = (0, 0, 0)
-        magtag.peripherals.neopixels[0] = (0, 0, 0)
         try:
             mqtt_client.is_connected()
         except MQTT.MMQTTException:
             logger.error("MQTT client is NOT connected")
             continue
-        # magtag.peripherals.neopixels.fill((0xFF, 0xFF, 0xFF))
         logger.debug(f"MQTT client connected")
-        magtag.peripherals.neopixels[1] = (0xFF, 0xFF, 0xFF)
         logger.debug("Starting MQTT client loop")
         # noinspection PyBroadException
         try:
-            mqtt_client.loop(75)
+            mqtt_client.loop(10)
         except Exception as loop_ex:  # catch *all* exceptions
             logger.error("Failed to get data; retrying")
             logger.error("%s: %s", type(loop_ex).__name__, loop_ex.args)
-            magtag.peripherals.neopixels[0] = (0xFF, 0, 0)
             # Don't resubscribe since the on_connect method always subscribes
             try:
                 mqtt_client.reconnect(resub_topics=False)
             except Exception as reconnect_ex:
                 logger.error("Failed to reconnect; resetting")
                 logger.error("%s: %s", type(reconnect_ex).__name__, reconnect_ex.args)
-                try:
-                    magtag.peripherals.neopixels[0] = (0xFF, 0, 0)
-                except Exception as neopixel_ex:
-                    logger.error("Failed to set NeoPixel to red while resetting; skipping")
-                    logger.error("%s: %s", type(neopixel_ex).__name__, neopixel_ex.args)
                 magtag.peripherals.deinit()
                 return
             continue
-
-        #TODO: Detect button presses by checking `magtag.peripherals.button_a_pressed` (or b, c, or d)
 
         logger.info("Sleeping for 60 seconds...")
         time.sleep(60)
